a083b6f7124fc0abf7df3198d1efc26dd744473f
Silsila <silsila@Silsilas-MacBook-Air.local>
2018-05-20 17:54:09 +0530

    Initial Commit

---
+++ iOSExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSExercise
+//
+//  Created by Silsila on 20/05/18.
+//  Copyright © 2018 Abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSExercise/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSExercise
+//
+//  Created by Silsila on 20/05/18.
+//  Copyright © 2018 Abc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
